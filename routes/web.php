98494e7f5e124e915c3ed80248326337d2453156
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-08-24 03:31:13 +0700

    sub criteria routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CriteriaController;
+use App\Http\Controllers\SubCriteriaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::resource('criteria', CriteriaController::class);
+Route::resource('sub-criteria', SubCriteriaController::class)->except(['index', 'show', 'create']);
