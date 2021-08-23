971e96f4241488e6708f21db97d30e3fa4459064
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-08-23 20:22:45 +0700

    rename model class to camel case style

--- app/Models/AlternativeValues.php
+++ app/Models/AlternativeValues.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class alternative_value extends Model
+class AlternativeValues extends Model
 {
     use HasFactory;
     public $timestamps = false;
--- app/Models/Alternatives.php
+++ app/Models/Alternatives.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class alternative extends Model
+class Alternatives extends Model
 {
     use HasFactory;
     public $timestamps = false;
