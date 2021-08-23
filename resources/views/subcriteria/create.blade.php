a63a395a5e8e1455d9bee55c6d661eae7d5e184c
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-08-24 03:32:28 +0700

    create sub criteria form

---
+++ resources/views/subcriteria/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.main')
+
+@section('header', 'Tambah Sub Kriteria')
+
+@section('section', 'Kriteria')
+
+@section('current-section', 'Tambah Sub Kriteria')
+
+@section('kriteria', 'active')
+
+@section('content')
+    <form action="{{ route('sub-criteria.store') }}" method="POST">
+        @csrf
+        <input type="hidden" value="{{ $id }}" name="criteria_id">
+        <div class="form-group">
+            <label>Nama Sub Kriteria</label>
+            <input type="text" class="form-control" name="sub_criteria_name" required>
+        </div>
+        <div class="form-group">
+            <label>Nilai</label>
+            <input type="text" min="0" class="form-control" name="value" required>
+        </div>
+        <button class="btn btn-primary" type="submit"><i class="fas fa-save"></i> Simpan</button>
+    </form>
+@endsection
